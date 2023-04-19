7e7a10488627411bcf2648f800dd22405913979e
HuaSheng <201172881@qq.com>
2023-04-19 10:21:51 +0800

    Initial Commit

---
+++ SwiftProjectStructure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftProjectStructure
+//
+//  Created by 花生 on 2023/4/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
